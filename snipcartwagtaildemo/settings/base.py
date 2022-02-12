56388ba04bb8c7521734842176000c0ca9384eac
Parker Johnson <johnson.parker7@gmail.com>
2022-02-12 13:39:05 -0800

    Update site structure for easy new pages

--- snipcartwagtaildemo/settings/base.py
+++ snipcartwagtaildemo/settings/base.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     'home',
     'search',
-
+    'store',
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
