0b27835f9b40b632945177d1f1fac8ae08c2cd78
Parker Johnson <johnson.parker7@gmail.com>
2022-02-16 10:01:25 -0800

    debugging

--- snipcartwagtaildemo/settings/dev.py
+++ snipcartwagtaildemo/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=1&k+m2ptz%&gsao!4*#&@b1r*i!e&=ci3@6(!vahn32%=(j9l'
